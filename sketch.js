1f0b06d33abd2d2db235bc61d905375eabc28978
Goldenfoxy523 <goldenfoxy523@gmail.com>
2020-12-22 14:54:44 -0500

    Included a game camera, changed the canvas size, and created the car sprites.

--- sketch.js
+++ sketch.js
@@ -8,9 +8,11 @@ var database;
 
 var form, player, game;
 
+var cars, car1, car2, car3, car4;
+
 
 function setup(){
-  canvas = createCanvas(400,400);
+  canvas = createCanvas(displayWidth-20,displayHeight-30);
   database = firebase.database();
   game = new Game();
   game.getState();
@@ -24,6 +26,7 @@ function draw(){
   }
   if(gameState === 1){
     clear();
+    console.log(player)
     game.play();
   }
 }
